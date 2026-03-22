dbb9c26577494a14447d8f903673ae2e60d80a3e
RexLiner <rexlinerpetergriffin@gmail.com>
2026-03-22 18:00:24 -0500

    new seeding conventions, users properly connected to backend

--- Selu383.SP26.Web/vite.config.ts
+++ Selu383.SP26.Web/vite.config.ts
@@ -2,5 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-    plugins: [react()],
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://localhost:7116', // 👈 your backend port here
+        changeOrigin: true,
+        secure: false, // needed if your backend uses a self-signed dev cert
+      }
+    }
+  }
 })
