cb90411cdad5843a02edf180c17a3cec37615f4f
RexLiner <rexlinerpetergriffin@gmail.com>
2026-04-06 14:12:35 -0500

    Add per-account points system with UserPoints table

---
+++ Selu383.SP26.Api/Migrations/20260406190930_AddUserPointsRelationship.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Selu383.SP26.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserPointsRelationship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_UserPoints_UserId",
+                table: "UserPoints");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserPoints_UserId",
+                table: "UserPoints",
+                column: "UserId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_UserPoints_UserId",
+                table: "UserPoints");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserPoints_UserId",
+                table: "UserPoints",
+                column: "UserId");
+        }
+    }
+}
