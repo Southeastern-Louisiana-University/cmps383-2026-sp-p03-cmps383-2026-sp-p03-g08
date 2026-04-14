bbf4ad15870cca5cb1c405ad61424b1f27ee1caa
evanlesage22 <evan.lesage@selu.edu>
2026-04-13 21:02:31 -0500

    add admin menu management and orders for the backend

---
+++ Selu383.SP26.Api/Features/Menu/MenuItemDto.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Selu383.SP26.Api.Features.Menu;
+
+public class MenuItemDto
+{
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(120)]
+    public string Name { get; set; } = string.Empty;
+
+    [Required]
+    [Range(0.01, double.MaxValue)]
+    public decimal Price { get; set; }
+
+    [Required]
+    [MaxLength(500)]
+    public string Description { get; set; } = string.Empty;
+}
