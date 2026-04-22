e637aef283593018ad033af6ca8494a43590e667
Lance Martin <lance.martin-2@selu.edu>
2026-04-22 18:39:06 -0500

    bjiewfjkewfjkcasdjlk

--- Selu383.SP26.Api/Features/Locations/LocationConfiguration.cs
+++ Selu383.SP26.Api/Features/Locations/LocationConfiguration.cs
@@ -10,10 +10,5 @@ public class LocationConfiguration : IEntityTypeConfiguration<Location>
         builder.Property(x => x.Name)
             .IsRequired()
             .HasMaxLength(120);
-
-        builder.Property(x => x.Settings)
-            .HasConversion(
-                v => System.Text.Json.JsonSerializer.Serialize(v, (System.Text.Json.JsonSerializerOptions)null),
-                v => System.Text.Json.JsonSerializer.Deserialize<Dictionary<string, bool>>(v, (System.Text.Json.JsonSerializerOptions)null));
     }
 }
