6700b5c69308702942b87b563e0184a323ab8e59
AXN-JXN <jackson.marble@selu.edu>
2026-04-22 17:05:10 -0500

    Phase 5 Changes

--- Selu383.SP26.Api/Features/Locations/LocationConfiguration.cs
+++ Selu383.SP26.Api/Features/Locations/LocationConfiguration.cs
@@ -10,5 +10,8 @@ public class LocationConfiguration : IEntityTypeConfiguration<Location>
         builder.Property(x => x.Name)
             .IsRequired()
             .HasMaxLength(120);
+
+        builder.Property(x => x.Settings)
+            .HasJsonConversion();
     }
 }
